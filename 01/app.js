82ed86b98e015fafa1c98e0eef166018e9310e27
Magdalena Kołtuniak <m.koltuniak@gmail.com>
2026-04-26 21:22:02 +0200

    add task 1

--- 01/app.js
+++ 01/app.js
@@ -4,14 +4,34 @@ import { createRoot } from 'react-dom/client';
 const root = createRoot(document.querySelector('#root'));
 
 class App extends React.Component {
-    state = {
-        counter: 0,
-    }
-    render() {
-        console.log('render');
+	state = {
+		counter: 0,
+	};
 
-        return <h1>{ this.state.counter }</h1>
-    }
+	render() {
+		console.log('render');
+
+		return <h1>{this.state.counter}</h1>;
+	}
+
+	componentDidMount() {
+		console.log('componentDidMount');
+
+		this.intervalId = setInterval(() => {
+			const { counter } = this.state;
+			this.setState(prev => ({ counter: prev.counter + 1 }));
+		}, 5000);
+	}
+
+	componentDidUpdate() {
+		console.log('componentDidUpdate');
+	}
+
+	componentWillUnmount() {
+		console.log('componentWillUnmount');
+
+		clearInterval(this.intervalId);
+	}
 }
 
-root.render(<App/>);
+root.render(<App />);
