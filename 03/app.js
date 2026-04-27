d57ef698d28f80ec244fc21b01f3d34667bf50d2
Magdalena Kołtuniak <m.koltuniak@gmail.com>
2026-04-27 15:54:53 +0200

    add task 3

--- 03/app.js
+++ 03/app.js
@@ -4,40 +4,72 @@ import { createRoot } from 'react-dom/client';
 const root = createRoot(document.querySelector('#root'));
 
 class Article extends React.Component {
-    state = {
-        comments: [],
-    }
-    
-    render() {
-        const {title, body} = this.props;
-        return (
-            <article>
-                <h1>{ title }</h1>
-                <p>{ body }</p>
-                <section>
-                    <form>
-                        <div>
-                            <label>
-                                <textarea 
-                                    style={{ "minWidth": "300px", "minHeight": "120px" }} 
-                                    name="content" 
-                                />
-                            </label>
-                        </div>
-                        <div><input type="submit" value="dodaj komentarz" /></div>
-                    </form>
-                    <ul>
-                        {/* tutaj komentarze jako <li/>, ps. tak wygląda komentarz do kodu w JSX */}
-                    </ul>
-                </section>
-            </article>
-        )
-    }
+	state = {
+		comments: [],
+		content: '',
+	};
+
+	render() {
+		const { title, body } = this.props;
+		const { comments, content } = this.state;
+
+		return (
+			<article>
+				<h1>{title}</h1>
+				<p>{body}</p>
+				<section>
+					<form onSubmit={this.submitHandler}>
+						<div>
+							<label>
+								<textarea
+									style={{ minWidth: '300px', minHeight: '120px' }}
+									name='content'
+									value={content}
+									onChange={this.changeHandler}
+								/>
+							</label>
+						</div>
+						<div>
+							<input type='submit' value='dodaj komentarz' />
+						</div>
+					</form>
+					<ul>{this.renderCommentsList()}</ul>
+				</section>
+			</article>
+		);
+	}
+
+	addComment = comment => {
+		this.setState(prev => ({
+			comments: [...prev.comments, comment],
+		}));
+	};
+
+	submitHandler = e => {
+		e.preventDefault();
+		const { comments, content } = this.state;
+		this.addComment(content);
+		this.setState({ content: '' });
+	};
+
+	changeHandler = e => {
+		const { name, value } = e.target;
+		this.setState({
+			[name]: value,
+		});
+	};
+
+	renderCommentsList = () => {
+		const { comments } = this.state;
+		return comments.map((comment, index) => {
+			return <li key={index}>{comment}</li>;
+		});
+	};
 }
 
 root.render(
-    <Article 
-        title="Programowanie jest super!"
-        body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis justo ipsum, eleifend vel quam eget, lobortis posuere arcu. In vitae eros in nisi sodales aliquam..."
-    />
+	<Article
+		title='Programowanie jest super!'
+		body='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis justo ipsum, eleifend vel quam eget, lobortis posuere arcu. In vitae eros in nisi sodales aliquam...'
+	/>,
 );
